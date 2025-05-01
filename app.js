e8b326cad382532aaf21cd39d5f8c3e6f0c92a36
TerynceChan <53795453+TerynceChan@users.noreply.github.com>
2025-05-01 17:12:19 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-import './backend/server.js';
+// importing modules
+import express from "express";
+import exphbs from "express-handlebars";
+import indexRoutes from "./routes/index.js";
+import { requestLogger } from "./middleware.js";
+
+const app = express();
+
+app.use("/public", express.static("public"));
+
+// json middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// express handlebars config
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use("/", requestLogger);
+
+// config app's routes
+configRoutesFunction(app);
+
+app.listen(3000, () => console.log("Server running at http://localhost:3000"));
